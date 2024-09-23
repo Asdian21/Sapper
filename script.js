a27549396ddb47bca9c3f98f36f047081be77c91
Asdian <asdiank7@gmail.com>
2024-09-23 18:11:23 +0500

    add async functions: sendRequest and authorization

---
+++ script.js
@@ -0,0 +1,67 @@
+async function sendRequest(url, method, data) {
+  url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`;
+
+  if (method == "POST") {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    response = await response.json();
+    return response;
+  } else if (method == "GET") {
+    url = url + "?" + new URLSearchParams(data);
+    let response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    return response;
+  }
+}
+
+let username;
+let balance;
+
+let authorizationForm = document.getElementById("authorization");
+authorizationForm.addEventListener("submit", authorization);
+
+async function authorization() {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  username = formData.get("userName");
+
+  let response = await sendRequest("user", "GET", { username });
+
+  if (response.error) {
+    // Пользователь не найден, нужно его зарегистрировать
+    let regResponse = await sendRequest("user", "POST", { username });
+    if (regResponse.error) {
+      //Возникла ошибка при регистрации
+      alert(regResponse.message);
+    } else {
+      //Успешная регистрация
+      balance = regResponse.balance;
+      showUser();
+    }
+  } else {
+    //Пользователь найден
+    balance = regResponse.balance;
+    showUser();
+  }
+}
+
+function showUser() {
+  let popUpSection = document.querySelector("section");
+  popUpSection.style.display = "none";
+  let userInfo = document.querySelector("header span");
+  userInfo.innerHTML = `[${username}, ${balance}]`;
+}
